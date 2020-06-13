fdde08d900c6e7a42a3ce7312c4ccd53c8c3b1d6
Federico Angioni <federicoangioni@Federicos-MBP.station>
2020-06-13 12:25:54 +0200

    script aggiornato

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,32 @@
+$(document).ready(function() {
+
+setInterval (function(){
+  $('.rightcolumn').addClass('enter-left');
+}, 2000);
+
+
+
+
+
 var cursor = $('.cursor');
 
 $( window ).on("mousemove", function( event ){
-cursor.css("left", event.pageX);
-cursor.css("right", event.pageY);
-});
+  cursor.css("left", event.pageX);
+  cursor.css("right", event.pageY);
+  });
 
 var li = $('span.monotone2');
 
 li.on("mouseenter", function( event ) {
-  cursor.toggleClass('big');
+  cursor.addClass('big');
 });
+
+li.on("mouseleave", function( event ) {
+  cursor.removeClass('big');
+});
+
+$( 'a' ).on( "click", function() {
+    $('body').fadeOut(1000);
+  });
+  
+}) ;
