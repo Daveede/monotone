de9e19446197b7a8a16b8fbc9ba21add67d2d15d
Federico Angioni <federicoangioni@Federicos-MBP.station>
2020-06-14 02:08:25 +0200

    bg white caricato con parent

--- js/bg-white.js
+++ js/bg-white.js
@@ -34,7 +34,7 @@ let particles = [];
 function setup() {
   var canvas;
   canvas = createCanvas(windowWidth, windowHeight);
-  canvas.position(0,0);
+  canvas.parent('bg-white');
   canvas.style('z-index', '0');
   for(let i = 0;i<width/6;i++){
     particles.push(new Particle());
